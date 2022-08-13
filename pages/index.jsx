1e9f7c982a24dcefb137aa473ccee555d0d27e79
ryzen666 <htoohtetaung26@gmail.com>
2022-08-13 17:40:13 +0630

    all basics of next js

---
+++ pages/index.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+      <h1 className={styles.title}>HomePage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <Link href="/ninjas">
+        <a className={styles.btn}>See Ninja List</a>
+      </Link>
+    </div>
+    </>
+    
+  )
+}
